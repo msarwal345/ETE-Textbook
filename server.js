863cf1abc93ab153b667b654218076de07b914d6
msarwal345 <mani.sarwal2003@gmail.com>
2024-02-13 00:30:12 +0530

    Minor Changes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post('/upload', upload.single('file'), controllers);
 app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.listen(8000, () => {
